27b0906fd1142fd6e56941e3dbdc30c20d2788fd
egor-cloud <egorcloud57@gmail.com>
2019-12-17 02:21:15 +0300

    2 commit

--- installTask_nodeJs/routes/task.js
+++ installTask_nodeJs/routes/task.js
@@ -1,17 +1,49 @@
 const router = require('express').Router()
 const Order = require('./../models/order')
-const Task = require('./../../handlerTask_nodeJs/models/task')
-const CompleteTask = require('./../../completeTask_nodeJs/models/completeTask')
+const handlers = require('./../../data/handlers.json')
+const complete = require('./../../data/completeHandlers.json')
+const fs = require('fs')
+const path = require('path')
 
 router.get('/', async (req, res) => {  
     const orders =  await Order.read_file_data_order()
-    const task = await Task.read_file_data_handler()
-    const completeTask = await CompleteTask.read_file_data_completeHandlers()
+    const task = await read_file_data_handler()
+    const completeTask = await read_file_data_completeHandlers()
     res.render('task',{
         orders,
         completeTask,
         task
     })
 })
+
+function read_file_data_handler(){
+    return new Promise((resolve, reject) =>{
+        fs.readFile(
+            path.join(__dirname, '..', '..', 'data', 'handlers.json'),
+            'utf-8',
+            (err, content) => {
+                if (err){
+                    reject(err)
+                }else{
+                    resolve(JSON.parse(content))
+                }
+            })          
+    })
+}
+
+function read_file_data_completeHandlers(){
+    return new Promise((resolve, reject) =>{
+        fs.readFile(
+            path.join(__dirname, '..', '..', 'data', 'completeHandlers.json'),
+            'utf-8',
+            (err, content) => {
+                if (err){
+                    reject(err)
+                }else{
+                    resolve(JSON.parse(content))
+                }
+            })          
+    })
+}
   
 module.exports = router
